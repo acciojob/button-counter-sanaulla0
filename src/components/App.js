02eec6880acde5d369ee6e921143d61ae3f5c14e
sanaulla0 <vadapallisanaulla@gmail.com>
2023-03-27 16:39:57 +0530

    p2

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [data,setData] = useState(0);
+  const handleClick = ()=>{
+       setData+=1;
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {data} times</p>
+
+<button type="submit" onClick="handleClick">Click Me</button>
+
     </div>
   )
 }
